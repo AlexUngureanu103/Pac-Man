dcc3d5d28370f752c86d38ba7211dddf31d441fc
CristiStroescu <stroescucristian478@yahoo.com>
2023-11-27 14:14:11 +0200

    MovePacManEvent - Created the Event for the movement of the Pac-Man

---
+++ Pac-Man/Pac-Man/Business/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pac_Man.Business
+{
+    public class InputManager
+    {
+        public event Action<char> OnKeyPressed;
+
+        public void HandleKeyPressed(char key)
+        {
+            OnKeyPressed?.Invoke(key);
+        }
+    }
+}
---
+++ Pac-Man/Pac-Man/ViewModels/GameWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Pac_Man.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pac_Man.ViewModels
+{
+    public class GameWindowViewModel
+    {
+        private readonly InputManager inputManager;
+
+        public GameWindowViewModel(InputManager inputManager) 
+        {
+            this.inputManager = inputManager;
+            this.inputManager.OnKeyPressed += GameWindowViewModel.HandleKeyPress;
+        }
+
+        public static void HandleKeyPress(char key)
+        {
+            switch (char.ToLower(key))
+            {
+                case 'w':
+                    // Send "Up" option to GameLogic from InputKeyEnum.cs
+                    break;
+                case 'a':
+                    // Send "Left" option to GameLogic from InputKeyEnum.cs
+                    break;
+                case 's':
+                    // Send "Down" option to GameLogic from InputKeyEnum.cs
+                    break;
+                case 'd':
+                    // Send "Right" option to GameLogic from InputKeyEnum.cs
+                    break;
+                default:
+                    // Send "Invalid" option to GameLogic from InputKeyEnum.cs
+                    break;
+            }
+        }
+    }
+}
