684d8ffec649c4f22716547b91454b53f48c73b3
CristiStroescu <stroescucristian478@yahoo.com>
2023-11-29 19:22:49 +0200

    PageNavigation - Removed the commands from LobbyWindowVM

--- Pac-Man/Pac-Man/ViewModels/LobbyWindowViewModel.cs
+++ Pac-Man/Pac-Man/ViewModels/LobbyWindowViewModel.cs
@@ -4,23 +4,6 @@ namespace Pac_Man.ViewModels
 {
     internal class LobbyWindowViewModel
     {
-        public ICommand PlayButtonCommand { get; }
-        public ICommand AboutButtonCommand { get; }
-
-        public LobbyWindowViewModel()
-        {
-            PlayButtonCommand = new Command(PlayButtonAction);
-            PlayButtonCommand = new Command(AboutButtonAction);
-        }
-
-        private void PlayButtonAction()
-        {
-            // Logic for Play Button action
-        }
-
-        private void AboutButtonAction()
-        {
-            // Logic for About Button action
-        }
+        
     }
 }
