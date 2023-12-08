701cc355cdc4e6997f8e12f990fd1cea2832e652
AlexUngureanu <a.ungureanu0099@gmail.com>
2023-12-08 11:48:54 +0200

    fix unit tests

--- Pac-Man/Pac-Man.Business.UnitTests/Movement/GhostAlgorithms/GhostPathAlgorithmsTests.cs
+++ Pac-Man/Pac-Man.Business.UnitTests/Movement/GhostAlgorithms/GhostPathAlgorithmsTests.cs
@@ -3,6 +3,7 @@ using Pac_Man.Business.GraphRepresentation;
 using Pac_Man.Business.Movement;
 using Pac_Man.Business.Movement.Ghost_Algorithms;
 using Pac_Man.Business.Movement.GhostAlgorithms;
+using Pac_Man.Domain;
 using Pac_Man.Domain.Models;
 
 namespace Pac_Man.Business.UnitTests.Movement.GhostAlgorithms
@@ -27,25 +28,25 @@ namespace Pac_Man.Business.UnitTests.Movement.GhostAlgorithms
         }
 
         [DataTestMethod]
-        [DataRow("Blinky", 8, 7, 9, 7, 9, 7)]
-        [DataRow("Blinky", 7, 12, 9, 11, 7, 13)]
-        [DataRow("Blinky", 7, 10, 9, 11, 7, 9)]
+        [DataRow(Ghosts.Blinky, 8, 7, 9, 7, 9, 7)]
+        [DataRow(Ghosts.Blinky, 7, 12, 9, 11, 7, 13)]
+        [DataRow(Ghosts.Blinky, 7, 10, 9, 11, 7, 9)]
 
-        [DataRow("Inky", 8, 7, 9, 7, 7, 7)]
-        [DataRow("Inky", 7, 11, 9, 11, 7, 10)]
-        [DataRow("Inky", 7, 12, 9, 11, 7, 11)]
-        [DataRow("Inky", 7, 13, 9, 11, 7, 12)]
-        [DataRow("Inky", 7, 14, 9, 11, 7, 15)]
+        [DataRow(Ghosts.Inky, 8, 7, 9, 7, 7, 7)]
+        [DataRow(Ghosts.Inky, 7, 11, 9, 11, 7, 10)]
+        [DataRow(Ghosts.Inky, 7, 12, 9, 11, 7, 11)]
+        [DataRow(Ghosts.Inky, 7, 13, 9, 11, 7, 12)]
+        [DataRow(Ghosts.Inky, 7, 14, 9, 11, 7, 15)]
 
-        [DataRow("Pinky", 8, 7, 9, 7, 9, 7)]
-        [DataRow("Pinky", 7, 11, 9, 11, 7, 12)]
-        [DataRow("Pinky", 7, 10, 9, 11, 7, 11)]
-        [DataRow("Pinky", 7, 9, 9, 11, 7, 10)]
-        [DataRow("Pinky", 7, 8, 9, 11, 7, 7)]
+        [DataRow(Ghosts.Pinky, 8, 7, 9, 7, 9, 7)]
+        [DataRow(Ghosts.Pinky, 7, 11, 9, 11, 7, 12)]
+        [DataRow(Ghosts.Pinky, 7, 10, 9, 11, 7, 11)]
+        [DataRow(Ghosts.Pinky, 7, 9, 9, 11, 7, 10)]
+        [DataRow(Ghosts.Pinky, 7, 8, 9, 11, 7, 7)]
 
-        [DataRow("Clyde", 8, 7, 9, 7, 7, 7)]
-        [DataRow("Clyde", 7, 12, 9, 11, 7, 13)]
-        [DataRow("Clyde", 7, 10, 9, 11, 7, 9)]
+        [DataRow(Ghosts.Clyde, 8, 7, 9, 7, 7, 7)]
+        [DataRow(Ghosts.Clyde, 7, 12, 9, 11, 7, 13)]
+        [DataRow(Ghosts.Clyde, 7, 10, 9, 11, 7, 9)]
         public void MainGhostMovements_ShouldReturnNextMove_WhenCalled(string ghostName, int ghostX, int gohstY, int playerX, int playerY, int expectedX, int expectedY)
         {
             var ghostPosition = new KeyValuePair<int, int>(ghostX, gohstY);
