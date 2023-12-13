062b14c0085a14d8c1442c6e5b1461148eaa51f9
CristiStroescu <stroescucristian478@yahoo.com>
2023-12-13 16:58:31 +0200

    CustomGridUI - Created the grid with the images

--- Pac-Man/Pac-Man/Pages/GamePage.xaml.cs
+++ Pac-Man/Pac-Man/Pages/GamePage.xaml.cs
@@ -1,5 +1,9 @@
 namespace Pac_Man.Pages;
+
 using Pac_Man.ViewModels;
+
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 
 public partial class GamePage : ContentPage
 {
@@ -11,6 +15,48 @@ public partial class GamePage : ContentPage
 
         Loaded += MainPage_Loaded;
         BindingContext = _gameWindowViewModel;
+
+        CreateImageGrid(23, 23);
+    }
+
+    private void CreateImageGrid(int rows, int columns)
+    {
+        var grid = new Grid();
+
+        int imageHeight = 14;
+        int imageWidth = 14;
+        int marginBetweenImages = 2;
+
+        grid.HeightRequest = rows * (imageHeight + marginBetweenImages);
+        grid.WidthRequest = columns * (imageWidth + marginBetweenImages);
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            grid.RowDefinitions.Add(new RowDefinition());
+        }
+
+        for (int i = 0; i < columns; i++)
+        {
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                var image = new Image();
+                image.Source = "pinky.png"; 
+                image.WidthRequest = 10; 
+                image.HeightRequest = 10; 
+
+                grid.Children.Add(image);
+                Grid.SetRow(image, i);
+                Grid.SetColumn(image, j);
+            }
+        }
+
+        mainStackLayout?.Children.Add(grid);
     }
 
     private void MainPage_Loaded(object sender, EventArgs e)
