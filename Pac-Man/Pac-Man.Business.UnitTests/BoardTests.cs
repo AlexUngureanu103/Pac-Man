ad42c4eca93f4202ce2351cb1ee3a84f5efec44f
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-05 00:48:40 +0200

    unit-tests - Added a unit test for Board.cs file

---
+++ Pac-Man/Pac-Man.Business.UnitTests/BoardTests.cs
@@ -0,0 +1,42 @@
+﻿using NSubstitute;
+using Pac_Man.Business.GraphRepresentation;
+
+namespace Pac_Man.Business.UnitTests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        private IBoard? board;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var gameCharacters = new GameCharacters();
+            board = Substitute.For<Board>(gameCharacters);
+            board.SmallerBoardGneration();
+        }
+
+        [DataTestMethod]
+        [DataRow("Clyde")]
+        [DataRow("Pinky")]
+        [DataRow("Inky")]
+        [DataRow("Blinky")]
+        public void Board_CheckIfGhostSeesThePlayer_ReturnFalseWhenGameStarts(string ghostName)
+        {
+            if (board is null)
+            {
+                Assert.Fail();
+            }
+
+            var result = board.CheckIfGhostSeesThePlayer(ghostName);
+
+            result.Should().BeFalse();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            board = null;
+        }
+    }
+}
