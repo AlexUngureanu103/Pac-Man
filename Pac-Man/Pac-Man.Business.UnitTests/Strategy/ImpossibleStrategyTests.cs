a2c89f3db48743ba8f409a7bc7ed825f0b3d0b21
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-05 11:06:29 +0200

    unit-tests - Created the test for ImpossibleStrategy

--- Pac-Man/Pac-Man.Business.UnitTests/Strategy/ImpossibleStrategyTests.cs
+++ Pac-Man/Pac-Man.Business.UnitTests/Strategy/ImpossibleStrategyTests.cs
@@ -1,6 +1,8 @@
 ﻿using NSubstitute;
 using Pac_Man.Business.Movement.Ghost_Algorithms;
 using Pac_Man.Business.Strategy;
+using Pac_Man.Domain.Models;
+using System;
 
 namespace Pac_Man.Business.UnitTests.Strategy
 {
@@ -8,24 +10,56 @@ namespace Pac_Man.Business.UnitTests.Strategy
     public class ImpossibleStrategyTests
     {
         private IStrategy? impossibleStrategy;
+        private IGhostPathAlgorithms? ghostPathAlgorithms;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            var ghostPathAlgorithms = Substitute.For<IGhostPathAlgorithms>();
+            ghostPathAlgorithms = Substitute.For<IGhostPathAlgorithms>();
             impossibleStrategy = Substitute.For<ImpossibleStrategy>(ghostPathAlgorithms);
         }
 
         [TestMethod]
-        public void ImpossibleStrategy_MoveGhosts_Returns()
+        public void ImpossibleStrategy_MoveGhosts_ReturnsNewPosition()
         {
+            Random random = new Random();
 
+            if (ghostPathAlgorithms == null || impossibleStrategy == null)
+                Assert.Fail();
+
+            var ghost = new MoveablesContainer(new Ghost())
+            {
+                position = new KeyValuePair<int, int>(11, 7)
+            };
+            var character = new MoveablesContainer(new Character())
+            {
+                position = new KeyValuePair<int, int>(1, 1)
+            };
+
+            int randomNumber = random.Next(2, 10);
+
+            for (int i=0; i<randomNumber; i++)
+            {
+                int randomNumber1 = random.Next(1, 20);
+                int randomNumber2 = random.Next(1, 20);
+
+                ghostPathAlgorithms.MainGhostMovements("Blinky", ghost, character).Returns(new KeyValuePair<int, int>(randomNumber1, randomNumber2));
+                impossibleStrategy.MoveGhosts(new KeyValuePair<string, MoveablesContainer>("Blinky", ghost), character);
+            }
+
+            ghostPathAlgorithms.MainGhostMovements("Blinky", ghost, character).Returns(new KeyValuePair<int, int>(11, 8));
+
+            var result = impossibleStrategy.MoveGhosts(new KeyValuePair<string, MoveablesContainer>("Blinky", ghost), character);
+
+            result.Key.Should().Be(11);
+            result.Value.Should().Be(8);
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
             impossibleStrategy = null;
+            ghostPathAlgorithms = null;
         }
     }
 }
